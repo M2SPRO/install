38722b95e11ff9fd8cde246a3f2498920b9b70a7
Marcio <121985280+M2SPRO@users.noreply.github.com>
2024-01-07 17:27:15 -0300

    Update int.sh

--- int.sh
+++ int.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-echo -ne "\033[1;36mINFORME SUA CHAVE!: \033[1;37m"; read chaveinstall
 apt update 
 apt upgrade -y
 apt install curl -y
-curl -o sshplus https://github.com/M2SPRO/install/$(uname -m)/sshplus
+curl -o sshplus https://github.com/M2SPRO/install/raw/main/$(uname -m)/sshplus
 chmod +x sshplus
 ./sshplus
-echo -n "$chaveinstall" > /opt/sshplus/licenca.txt
+rm sshplus
